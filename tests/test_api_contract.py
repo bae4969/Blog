92ecda5920f02892d8d7bdb533341c31b8e08905
bae4969 <bae4969@naver.com>
2026-09-12 17:42:32 +0900

    CI 라우트 검사를 FastAPI 버전과 호환한다

--- tests/test_api_contract.py
+++ tests/test_api_contract.py
@@ -500,7 +500,7 @@ class TestStockPriceSource:
             assert marker in template
         assert 'id="stockRows"' not in template
         assert '<a href="/quotes">지수·환율</a>' not in layout
-        assert all(route.path != "/quotes" for route in app.routes)
+        assert all(getattr(route, "path", None) != "/quotes" for route in app.routes)
 
 
 class TestSessionTokenBridge:
